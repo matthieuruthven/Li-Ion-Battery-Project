2c00369a708afa7ecd869982db2443792de33445
Matthieu Ruthven <matthieuruthven@nhs.net>
2024-03-19 12:27:11 +0100

    Updated to include simulation of spatial anomalies

--- Energetic_Generate_Dataset.m
+++ Energetic_Generate_Dataset.m
@@ -1,4 +1,4 @@
-function Energetic_Generate_Dataset(sim_type, sim_time, C_rate, k, h, dirpath, sq_wave_period, img_syn_int)
+function Energetic_Generate_Dataset(sim_type, sim_time, batt_type, anomaly, C_rate, k_in, h_in, T0_in, dirpath, sq_wave_period, img_syn_int, SOC, def_or_cus)
 % Energetic_Generate_Dataset.m
 % Function to generate dataset of synthetic thermal images using
 % computational model developed by Lin et al. (2022)
@@ -12,23 +12,35 @@ function Energetic_Generate_Dataset(sim_type, sim_time, C_rate, k, h, dirpath, s
 % discharged or both in the simulation (options: Charge, Discharge or 
 % Square)
 % sim_time (integer): the duration of the simulation in seconds
+% batt_type (string): specifies the configuration and dimensions of the
+% battery (options: Energetic or Lin)
+% anomaly (string): specifies if there should be an anomaly in the thermal
+% conductivity of the battery (options: None, Circle, Stripe_W, Stripe_H)
 % C_rate (float): the C-rate at which the pouch cell is (dis)charged at
-% k (float): the thermal conductivity of the pouch cell in W/m/K
-% h (float): the heat transfer coefficient of the pouch cell in W/m^2/K
+% k_in (float): the thermal conductivity of the pouch cell in W/m/K
+% h_in (float): the heat transfer coefficient of the pouch cell in W/m^2/K
+% T0_in (float): the ambient temperature and the initial temperature of the
+% pouch cell
 % dirpath (string): the path to the folder in which the dataset should be
 % saved
 % sq_wave_period (integer): the duration of the square wave in seconds (only
 % relevant when the sim_type argument is 'Square')
 % img_syn_int (integer): the time interval between synthesis of
 % consecutive images
+% SOC (float): the initial state of charge (SOC) of the positive electrode
+% (options: 0.3, 0.5 or 0.7)
+% def_or_cus (string): specifies whether to use custom values of k, h and 
+% T0 (i.e. the values of the k, h and T0 arguments input to the function) 
+% or default ones (i.e. the k, h and T0 arguments input to the function 
+% will be overridden) (options: custom or default)
 
 % Author: Matthieu Ruthven (matthieu.ruthven@uni.lu)
-% Last modified: 23rd February 2024
+% Last modified: 18th March 2024
 
 % Start timing
 tic
 
-% If sim_type is either Charge or Discharge, sq_wave_period should be 2 * 
+% If sim_type is either Charge or Discharge, sq_wave_period should be 2 *
 % sim_time
 if strcmp(sim_type, 'Charge') || strcmp(sim_type, 'Discharge')
     sq_wave_period = 2 * sim_time;
@@ -40,38 +52,101 @@ else
     error('"sim_type" argument should be "Charge", "Discharge" or "Square"')
 end
 
-% Vector of battery model parameter values
-% Values are those from Table S3 in Lin et al. (2022)
-x0 = [1.0220    1.1000     12.0000     0.1800     81.0000       4.2000     6.0000     29.5000   4.5000];
-x = x0;
-% NB
-% x0(1) is 2.35 * 10^6 / 2300 [Cp in Table S3] / [electrode density]
-% x0(4) is ku 70% SOC in Table 1
-% x0(5) is 0.046 * 42^2 [kappa in Table S3] * [number of cell layers]^2
-% x0(6) is 2.4 * 10^-3 * 42^2 [alpha in Table S3] * [number of cell layers]^2
+% Check battery
+if strcmp(batt_type, 'Energetic') || strcmp(batt_type, 'Lin')
+else
+    error('"batt_type" argument should be either "Energetic" or "Lin"')
+end
 
-% Extract battery model parameter values from vector x
-% Code source: https://github.com/Battery-Intelligence-Lab/multiscale-coupling/blob/dba3649f2eeb4228d2849e4d4e748c91ed6ad7a4/ObjFunc.m#L5C1-L17C48
+% Check anomaly
+if strcmp(anomaly, 'None') || strcmp(anomaly, 'Circle')
+elseif strcmp(anomaly, 'Stripe_H') || strcmp(anomaly, 'Stripe_W')
+else
+    error('"anomaly" argument should be "None", "Circle", "Stripe_H" or "Stripe_W"')
+end
+
+% Battery model parameter values
+% NB
+% 2.35 * 10^6 / 2300 [Cp in Table S3] / [electrode density]
+% ku 70% SOC in Table 1
+% 0.046 * 42^2 [kappa in Table S3] * [number of cell layers]^2
+% 2.4 * 10^-3 * 42^2 [alpha in Table S3] * [number of cell layers]^2
+
+
 tf          = round(sim_time);       % Total simulation time 
-Cp          = x(1)*1e3;   % Specific heat
-% k           = x(2);       % Thermal conductivity
-% h           = x(3);       % Heat transfer coefficient   
-ku          = x(4);       % OCP gradient
-kappa       = x(5);       % Ionic conductivity
-alfa_ka     = x(6);       % Temperature coefficient of kappa
-i0ref       = x(7);       % Exchange current density
-Ei0         = x(8);       % Reaction activation energy
-Ds          = x(9)*1e-14; % Diffusion coeffcient
-DS          = -0.10;      % Entropy change [Entropy change 70% SOC in Table 1 / Faraday constant * 1000] NB * 1000 to convert to mV per K
-Vinit       = 3.2381;      % Initial cell voltage
-T0          = 23.85;      % Ambient temperature
+Cp          = 1022;                  % Specific heat (derived from Table S6)
+i0ref       = 6;                     % Exchange current density (From Table S6)
+Vinit       = 3.2381;                % Initial cell voltage (from original code)
+if strcmp(sim_type, 'Square')
+    if SOC == 0.3
+        k = 1.1;         % Thermal conductivity
+        h = 12.0;        % Heat transfer coefficient
+        ku = 0.35;       % OCP gradient
+        kappa = 81.1;    % Ionic conductivity
+        alfa_ka = 4.2;   % Temperature coefficient of kappa
+        Ei0 = 29.5;      % Reaction activation energy
+        Ds = 4.5*1e-14;  % Diffusion coeffcient
+        DS = -0.14;      % Entropy change
+        T0 = 23.85;      % Ambient temperature
+    elseif SOC == 0.5
+        k = 1.2;         % Thermal conductivity
+        h = 11.9;        % Heat transfer coefficient
+        ku = 0.24;       % OCP gradient
+        kappa = 75.9;    % Ionic conductivity
+        alfa_ka = 4.8;   % Temperature coefficient of kappa
+        Ei0 = 29.2;      % Reaction activation energy
+        Ds = 4.2*1e-14;  % Diffusion coeffcient
+        DS = 0.08;       % Entropy change
+        T0 = 21.95;      % Ambient temperature
+    elseif SOC == 0.7
+        k = 1.1;         % Thermal conductivity
+        h = 11.8;        % Heat transfer coefficient 
+        ku = 0.18;       % OCP gradient
+        kappa = 79.4;    % Ionic conductivity
+        alfa_ka = 4.2;   % Temperature coefficient of kappa
+        Ei0 = 29.7;      % Reaction activation energy
+        Ds = 4*1e-14;    % Diffusion coeffcient
+        DS = 0.10;       % Entropy change
+        T0 = 25.35;      % Ambient temperature
+    else
+        error('"SOC" argument should be 0.3, 0.5 or 0.7')
+    end
+else
+    k = 1.1;         % Thermal conductivity
+    h = 12.0;        % Heat transfer coefficient
+    ku = 0.35;       % OCP gradient (NB will be overridden)
+    kappa = 81.1;    % Ionic conductivity
+    alfa_ka = 4.2;   % Temperature coefficient of kappa
+    Ei0 = 29.5;      % Reaction activation energy
+    Ds = 4.5*1e-14;  % Diffusion coeffcient
+    DS = -0.14;      % Entropy change (NB will be overridden)
+    T0 = 23.85;      % Ambient temperature
+end
+
+% If required, change values of k, h and T0
+if strcmp(def_or_cus, 'custom')
+    k = k_in;         % Thermal conductivity
+    h = h_in;         % Heat transfer coefficient
+    T0 = T0_in;       % Ambient temperature
+elseif strcmp(def_or_cus, 'default')
+else
+    error('"def_or_cus" argument should be either "custom" or "default"')
+end
 
 % Create folder in which dataset will be saved (an error will be raised if
 % the folder already exists)
 if strcmp(sim_type, 'Charge') || strcmp(sim_type, 'Discharge')
-    dirpath = fullfile(dirpath, strrep(sprintf('C_rate_%.02f_T_amb_%.02f_h_%.01f_k_%.01f_%s', C_rate, T0, h, k, sim_type), '.', '_'));
+    if strcmp(anomaly, 'None')
+        dirpath = fullfile(dirpath, strrep(sprintf('%s_C_rate_%.02f_T_amb_%.02f_h_%.01f_k_%.01f_%s_SOC_%.01f', batt_type, C_rate, T0, h, k, sim_type, SOC), '.', '_'));
+    else
+        dirpath = fullfile(dirpath, strrep(sprintf('%s_C_rate_%.02f_T_amb_%.02f_h_%.01f_k_%.01f_%s_SOC_%.01f_Anomaly_%s', batt_type, C_rate, T0, h, k, sim_type, SOC, anomaly), '.', '_'));
+    end    
 else
-    dirpath = fullfile(dirpath, strrep(sprintf('C_rate_%.02f_T_amb_%.02f_h_%.01f_k_%.01f_%s_%d', C_rate, T0, h, k, sim_type, round(sq_wave_period)), '.', '_'));
+    if strcmp(anomaly, 'None')
+        dirpath = fullfile(dirpath, strrep(sprintf('%s_C_rate_%.02f_T_amb_%.02f_h_%.01f_k_%.01f_%s_%d_SOC_%.01f', batt_type, C_rate, T0, h, k, sim_type, round(sq_wave_period), SOC), '.', '_'));
+    else
+        dirpath = fullfile(dirpath, strrep(sprintf('%s_C_rate_%.02f_T_amb_%.02f_h_%.01f_k_%.01f_%s_%d_SOC_%.01f_Anomaly_%s', batt_type, C_rate, T0, h, k, sim_type, round(sq_wave_period), SOC, anomaly), '.', '_'));
+    end
 end
 [status, msg] = mkdir(dirpath);
 if or(status ~= 1, contains(msg,'already')) 
@@ -79,8 +154,7 @@ if or(status ~= 1, contains(msg,'already'))
 end
 
 % Send battery model to COMSOL server
-% Code source: https://github.com/Battery-Intelligence-Lab/multiscale-coupling/blob/dba3649f2eeb4228d2849e4d4e748c91ed6ad7a4/ObjFunc.m#L20
-out = Battery_model_energetic(sim_type,round(sq_wave_period),round(img_syn_int),C_rate,dirpath,tf,T0,Cp,k,h,ku,kappa,alfa_ka,i0ref,Ei0,Ds,DS,Vinit);
+out = Battery_model_energetic(sim_type,round(sq_wave_period),round(img_syn_int),batt_type,anomaly,C_rate,dirpath,tf,T0,Cp,k,h,ku,kappa,alfa_ka,i0ref,Ei0,Ds,DS,Vinit,SOC);
 
 % Finish timing
 toc
